6afbda76f20288dd4b0329434e3dbd15784099c1
Seb Pretzer <24555985+sebpretzer@users.noreply.github.com>
2025-07-22 12:11:09 -0500

    silence integration test warnings

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,3 +1,4 @@
+import warnings
 from pathlib import Path
 
 import pytest
@@ -7,6 +8,12 @@ from pyticktick import Client
 
 @pytest.fixture()
 def client() -> Client:
-    if not (env_file_path := Path(__file__).parents[2].joinpath(".env")).exists():
-        return Client()
-    return Client(_env_file=env_file_path)  # pyright: ignore[reportCallIssue] # https://github.com/pydantic/pydantic/issues/3072
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "error",
+            message="`token` will expire in less than 7 days",
+        )
+        if not (env_file_path := Path(__file__).parents[2].joinpath(".env")).exists():
+            return Client()
+        return Client(_env_file=env_file_path)  # pyright: ignore[reportCallIssue]
+        # https://github.com/pydantic/pydantic/issues/3072
